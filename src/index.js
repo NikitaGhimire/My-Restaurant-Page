67a1f1078f1c2f7303c35a32b6264b28073b4960
NikitaGhimire <nikitaghimire1@gmail.com>
2024-05-01 10:09:26 +1000

    wrote the tab-switching logic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
+import { loadContactPage } from "./contact-page";
+import { loadMenuPage } from "./menu-page";
+import { loadHomePage } from "./home-page";
+import { loadReservationPage } from "./reservation-page";
 
+document.addEventListener('DOMContentLoaded', () => {
+  loadHomePage();
+
+  const homeTab = document.getElementById('home-tab');
+  const menuTab = document.getElementById('menu-tab');
+  const reservationTab = document.getElementById('reservation-tab');
+  const contactTab = document.getElementById('contact-tab');
+
+  homeTab.addEventListener('click', () => {
+    clearContent();
+    loadHomePage();
+  });
+
+  menuTab.addEventListener('click', () => {
+    clearContent();
+    loadMenuPage();
+  });
+
+  reservationTab.addEventListener('click', () => {
+    clearContent();
+    loadReservationPage();
+  });
+
+  contactTab.addEventListener('click', () => {
+    clearContent();
+    loadContactPage();
+  });
+
+  function clearContent() {
+    const content = document.getElementById('content');
+    content.innerHTML = '';
+  }
+});
