406d6e5c2e86d3fa8e37a254a8697cf68f5d2b99
NikitaGhimire <nikitaghimire1@gmail.com>
2024-05-01 10:10:10 +1000

    fixed naming errors

--- src/reservation-page.js
+++ src/reservation-page.js
@@ -1,8 +1,8 @@
-export function loadMenuPage(){
+export function loadReservationPage(){
   const content = document.getElementById('content');
-  const menuContent = document.createElement('div');
-  menuContent.innerHTML = `
+  const reservationContent = document.createElement('div');
+  reservationContent.innerHTML = `
    <h1> Make your reservations here </h1>
   `;
-  content.appendChild(menuContent);
+  content.appendChild(reservationContent);
 }
